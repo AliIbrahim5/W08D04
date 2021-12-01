8a282645bd8fa66f079b8cd74adcf5717f0c7102
Aliibrahim5 <Ali.ibrahim.elq.js@tuwaiq.edu.sa>
2021-12-01 14:22:04 +0300

    user controllers and routes

--- routers/controllers/user.js
+++ routers/controllers/user.js
@@ -1,14 +1,8 @@
-// تسمية يوزر مودل  واخذه من مجلد المودل يوزر
 const usermodel = require("../../db/models/user");
-// لتشفير الباسورد للحماية من السرقة
 const bcrypt = require("bcrypt");
-// يتم استعماله لتغير او تشفير الباس كل ماسجلت دخول للحماية من السرقة
 const jwt = require("jsonwebtoken");
-// السولت لتشفير التسجيل
 const salt = Number(process.env.SALT);
-// تابع jwt
 const secret = process.env.SECRET;
-// كنترول اليوزر لتسجيل
 const resgister = async (req, res) => {
   const { email, password, role } = req.body;
 
@@ -30,44 +24,7 @@ const resgister = async (req, res) => {
     });
 };
 
-// كنترول الدخول لليوزر
-// const login = (req, res) => {
-//   const { email, password } = req.body;
-//   const savedEmail = email.toLowerCase();
 
-//   usermodel
-//     .findOne({ email: savedEmail })
-//     .then(async (result) => {
-//       if (result) {
-//         if (result.email == email) {
-//           const savedPassword = await bcrypt.compare(
-//             password,
-//             result.password,
-//             options
-//           );
-//           const payload = {
-//             email,
-//           };
-//           if (savedPassword) {
-//             const token = jwt.sign(payload, secret);
-
-//             res.status(200).json({ result, token });
-//           } else {
-//             res.status(400).json("Wrong email or password");
-//           }
-//         } else {
-//           res.status(400).json("Wrong email or password");
-//         }
-//       } else {
-//         res.status(404).json("Email not exist");
-//       }
-//     })
-//     .catch((err) => {
-//       res.send(err);
-//     });
-// };
-
-// كنترول الدخول لليوزر
 const login = (req, res) => {
   const { email, password } = req.body;
   const savedEmail = email.toLowerCase();
@@ -123,5 +80,5 @@ const deletuser = (req, res) => {
       res.json(err);
     });
 };
-// اكسبورت لتسجيل والدخول
+
 module.exports = { resgister, login, getalluser, deletuser };
