59d6c2c3f1744065e4cd4fa5b14b3d213d9e43f4
Aliibrahim5 <Ali.ibrahim.elq.js@tuwaiq.edu.sa>
2021-12-14 16:58:14 +0300

    ffgdf

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 dotenv.config();
 app.use(
   cors({
-    origin: "https://socialmedia-2000.herokuapp.com", 
+    // origin: "http://localhost:3000", 
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
   })
